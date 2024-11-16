e0c7ece8b0b9f66245ff16fb3ba36ac127863fc3
Tanmay Das <tanmaymishu@gmail.com>
2024-11-17 04:10:41 +0600

    Minify assets

--- bootstrap/ssr/ssr.js
+++ bootstrap/ssr/ssr.js
@@ -1,30 +1,21 @@
-import { jsx, jsxs } from "react/jsx-runtime";
+import { jsx } from "react/jsx-runtime";
 import { createInertiaApp } from "@inertiajs/react";
 import createServer from "@inertiajs/react/server";
 import ReactDOMServer from "react-dom/server";
-const Index = () => {
-  return /* @__PURE__ */ jsx("div", { children: /* @__PURE__ */ jsx("h1", { children: "Index Page. This is the index page" }) });
+const Index = (props) => {
+  console.log(props);
+  return /* @__PURE__ */ jsx("div", { className: "flex flex-col h-screen justify-center items-center", children: "Lemmego [inertia-react]" });
 };
 const __vite_glob_0_0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   default: Index
-}, Symbol.toStringTag, { value: "Module" }));
-const Welcome = (props) => {
-  return /* @__PURE__ */ jsx("div", { children: /* @__PURE__ */ jsxs("h1", { children: [
-    "Welcome Page. This is the welcome page ",
-    props.name
-  ] }) });
-};
-const __vite_glob_0_1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
-  __proto__: null,
-  default: Welcome
 }, Symbol.toStringTag, { value: "Module" }));
 createServer(
   (page) => createInertiaApp({
     page,
     render: ReactDOMServer.renderToString,
     resolve: (name) => {
-      const pages = /* @__PURE__ */ Object.assign({ "./Pages/Home/Index.tsx": __vite_glob_0_0, "./Pages/Home/Welcome.tsx": __vite_glob_0_1 });
+      const pages = /* @__PURE__ */ Object.assign({ "./Pages/Index.tsx": __vite_glob_0_0 });
       return pages[`./Pages/${name}.tsx`];
     },
     setup: ({ App, props }) => /* @__PURE__ */ jsx(App, { ...props })
